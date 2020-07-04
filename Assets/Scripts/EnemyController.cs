344da262ffa67a5c40ea0075fdce9861b4021231
Lukas077 <lukaszklosek@microsoft.wsei.edu.pl>
2020-07-04 13:31:44 +0200

    AI

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,65 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class EnemyController : MonoBehaviour
 {
-
-    private NavMeshAgent _navMeshAgent;
-    private Animator _animator;
-    public GameObject Player;
-    public float AttackDistance = 10.0f;
-    public float FollowDistance = 20.0f;
-
-    [Range(0.0f, 1.0f)] public float AttackProbability = 0.5f;
+  
     
-
-   
-    void Start ()
-        {
-
-        _navMeshAgent = GetComponent<NavMeshAgent>();
-        _animator = GetComponent<Animator>();
-        
-        }
-	
-    void Update ()
-    {
-        if (_navMeshAgent.enabled)
-        {
-            float dist = Vector3.Distance(Player.transform.position, this.transform.position);
-            bool follow = (dist < FollowDistance);
-            bool hit = false;
-            if (follow)
-            {
-                float random = Random.Range(0.0f, 1.0f);
-                if (random > (1.0f - AttackProbability) && dist < AttackDistance)
-                {
-                    hit = true;
-                }
-            }
-
-            if (follow)
-            {
-                _navMeshAgent.SetDestination(Player.transform.position);
-            }
-
-            if (!follow || hit)
-            {
-                _navMeshAgent.SetDestination(transform.position);
-                
-                _animator.SetBool("Hit", hit);
-                _animator.SetBool("Move", follow);
-            }
-        }
-
-
-
-
-
-
-    }
 
 
 }
